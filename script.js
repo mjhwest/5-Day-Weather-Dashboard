9b333870eee7c5836289c875f8ed3b4297d3cc07
mjhwest89 <mjhwest89@gmail.com>
2021-10-13 22:34:09 +1030

    many changes

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 var weatherAPI = "81bba03d80a285cb4521ac469ecbb174";
 var cityDetailContainer = $('#weatherDisplayData');
-// var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+var searchButton = $('#button-addon2');
+
 
 let city = '';
 let searchBtn = $('#button-addon2');
 searchBtn.on('click', function() {
     let city = $('#newCity').val().toLowerCase();
-    currentWeather(city)
+    getData(city)
 })
 
+
+//function to get the current weather for a city 
 function currentWeather(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
     $.ajax({
         url: queryURL,
         method: 'GET',
-    }).then(function(response) {
-        console.log('Ajax Response \n-------------');
-        console.log(response);
-    });
+    }).then(function(currentCityWeather) {
+        console.log(currentCityWeather);
 
+     $('#weatherDisplayData').css("display", "block");
+     $('#cityDeails').empty();
+
+    });
 }
 
-function getData() {
+
+//function to display the weather 
+function getData(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
     fetch(queryURL)
         .then(function(response) {
@@ -37,14 +44,9 @@ function getData() {
             const cityWind = $("<p>");
             //setting the text of h3 and p
             cityName.text(data.name)
-            cityTemp.text(data.temp)
-            cityHumidity.text(data.humidity)
-                //cityWind.text(data[i].wind.speed) remove the i as its not in a loop
-            cityWind.text(data.wind.speed)
-                // cityName.text() = data[i].name;
-                // cityTemp.text() = data[i].temp;
-                // cityHumidity.text() = data[i].humidity;
-                // cityWind.text() = data[i].wind.speed;
+            cityTemp.text('Tempterature: ' + data.main.temp + 'C')
+            cityHumidity.text('Humidity: ' + data.main.humidity + '%')
+            cityWind.text('Wind Speed: ' + data.wind.speed + 'Km/h')
                 //Appending the dynamically generated html to the div associated with the id="users"
                 //Append will attach the element as the bottom most child.
             cityDetailContainer.append(cityName);
@@ -52,8 +54,25 @@ function getData() {
             cityDetailContainer.append(cityHumidity);
             cityDetailContainer.append(cityWind);
         });
+        searchButton.on('click', getData);
 }
 
+
+//function to display UV index: 
+// function uvIndexData(city) 
+//     var uvAPI = `https://api.openweathermap.org/data/2.5/solar_radiation?lat=${lat}&lon=${lon}&appid=${weatherAPI}`
+//     fetch(uvAPI)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     })
+
+// const lat = data.coord.lat;
+// const lon = data.coord.lon; 
+
+
 //Call functions
-currentWeather('adelaide');
-getData();
+// currentWeather('adelaide');
+// getData();
