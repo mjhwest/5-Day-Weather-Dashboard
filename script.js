6be79ae0b97e0e1e3a7aa1c792efb7d3c7e30f72
mjhwest89 <mjhwest89@gmail.com>
2021-10-13 14:13:12 +1030

    update, more js done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var weatherAPI = "81bba03d80a285cb4521ac469ecbb174";
-
-// var city = 'adelaide'
-
+var cityDetailContainer = $('#weatherDisplayData');
 // var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-// var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
-
-
-
+let city = '';
+let searchBtn = $('#button-addon2');
+searchBtn.on('click', function() {
+    let city = $('#newCity').val().toLowerCase();
+    currentWeather(city)
+})
 
 function currentWeather(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
@@ -21,4 +21,39 @@ function currentWeather(city) {
 
 }
 
+function getData() {
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
+    fetch(queryURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            //create h3 element for city name an p elements for displaying info 
+            const cityName = $("<h3>");
+            const cityTemp = $("<p>");
+            const cityHumidity = $("<p>");
+            const cityWind = $("<p>");
+            //setting the text of h3 and p
+            cityName.text(data.name)
+            cityTemp.text(data.temp)
+            cityHumidity.text(data.humidity)
+                //cityWind.text(data[i].wind.speed) remove the i as its not in a loop
+            cityWind.text(data.wind.speed)
+                // cityName.text() = data[i].name;
+                // cityTemp.text() = data[i].temp;
+                // cityHumidity.text() = data[i].humidity;
+                // cityWind.text() = data[i].wind.speed;
+                //Appending the dynamically generated html to the div associated with the id="users"
+                //Append will attach the element as the bottom most child.
+            cityDetailContainer.append(cityName);
+            cityDetailContainer.append(cityTemp)
+            cityDetailContainer.append(cityHumidity);
+            cityDetailContainer.append(cityWind);
+        });
+}
+
+//Call functions
 currentWeather('adelaide');
+getData();
