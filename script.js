446b6f5cd2101e0ee70654cbaea7ed9f1b619879
mjhwest89 <mjhwest89@gmail.com>
2021-10-17 17:24:53 +1030

    added local store

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-var weatherAPI = "81bba03d80a285cb4521ac469ecbb174";
+// var weatherAPI = "81bba03d80a285cb4521ac469ecbb174"; re use this key tomorrow, currently over limit
+var weatherAPI = "9e0ede6ed9dad7c9f716f87588160be2";
 var cityDetailContainer = $('#weather-container');
 var searchButton = $('#button-addon2');
 var forecastContainer = $('#day-forecast');
-//need to add DATE in still
+var citySearch = $('#newCity')
 
-// let city = '';
-// let searchBtn = $('#button-addon2');
-// searchBtn.on('click', function() {
-//     let city = $('#newCity').val().toLowerCase();
-//     getData(city)
-// })
+var searchLog = []; //not sure if needed 
 
-//function to get the current weather for a city 
-// function currentWeather(city) {
-//     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherAPI}`
-//     $.ajax({
-//         url: queryURL,
-//         method: 'GET',
-//     }).then(function(currentCityWeather) {
-//         console.log(currentCityWeather);
-
-//         $('#weatherDisplayData').css("display", "block");
-//         $('#cityDeails').empty();
-
-//     });
-// }
-
-//function to display the weather 
-
+// run each function on button click
+searchButton.on('click', function() {
+    let city = $('#newCity').val().toLowerCase();
+    let cityList = city.charAt(0).toUpperCase() + city.slice(1);
+    fiveDayForecast(city)
+    getData(city);
+    console.log(city)
+    storeCityHistory(cityList);
+    $('#searchResult').append(`<li class ="searched" > ${cityList}</li>`)
+})
 
 
 function getData(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherAPI}`
+    console.log(queryURL)
     fetch(queryURL)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data);
-
             //THIS IS NOT WORKING
             // let currentCol = $(`
             // <div class = "col" id="day-weather">
@@ -50,38 +39,50 @@ function getData(city) {
             // `);
             // $('.current').append(currentCol); // Append current weather div to current row
 
-            //create h3 element for city name an p elements for displaying info 
-            const cityName = $("<h3>");
-            const cityTemp = $("<p>");
-            const cityHumidity = $("<p>");
-            const cityWind = $("<p>");
-            //setting the text of h3 and p
-            cityName.text('Currently in ' + data.name)
-            cityTemp.text('Tempterature: ' + data.main.temp + 'C')
-            cityHumidity.text('Humidity: ' + data.main.humidity + '%')
-            cityWind.text('Wind Speed: ' + data.wind.speed + 'Km/h')
-                //Appending the dynamically generated html to the div associated with the id="users"
-                //Append will attach the element as the bottom most child.
-            cityDetailContainer.append(cityName);
-            cityDetailContainer.append(cityTemp)
-            cityDetailContainer.append(cityHumidity);
-            cityDetailContainer.append(cityWind);
-            //setting the data for uv index, need lon and lat
-            let lon = data.coord.lon;
-            let lat = data.coord.lat;
-            uvIndexData(lon, lat)
+
+            if ($('.main').length == 0) { //if class=main dosent exist append data 
+                //create h3 element for city name an p elements for displaying info 
+                let cityName = $("<h3 class='main'>");
+                let cityTemp = $("<p>");
+                let cityHumidity = $("<p>");
+                let cityWind = $("<p>");
+                //setting the text of h3 and p
+                cityName.text('Currently in ' + data.name)
+                cityTemp.text('Tempterature: ' + data.main.temp + 'C')
+                console.log(cityTemp.text())
+                cityHumidity.text('Humidity: ' + data.main.humidity + '%')
+                cityWind.text('Wind Speed: ' + data.wind.speed + 'Km/h')
+                    //dynamically appending text
+                cityDetailContainer.append(cityName);
+                cityDetailContainer.append(cityTemp)
+                cityDetailContainer.append(cityHumidity);
+                cityDetailContainer.append(cityWind);
+
+                let lon = data.coord.lon;
+                let lat = data.coord.lat;
+                // uvIndexData(lon, lat)
+
+
                 //setting the date using unix
-            const dayDate = moment.unix(data.dt).format("dddd, MMMM Do YYYY")
-            console.log(dayDate)
-            cityName.append(': ' + dayDate)
+                const dayDate = moment.unix(data.dt).format("dddd, MMMM Do YYYY")
+                console.log(dayDate)
+
+
                 //creating variable to display current weather icon
-            var iconCode = data.weather[0].icon;
-            var iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
-            cityName.after(`<img src="${iconURL}" class"icon-img" alt="icon image">`)
+                var iconCode = data.weather[0].icon;
+                var iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
 
-
+                cityName.append(': ' + dayDate)
+                cityName.after(`<img src="${iconURL}" class"icon-img" alt="icon image">`)
+                uvIndexData(lon, lat)
+                    // console.log('does exist')
+            } else { //if it does exist, 
+                cityDetailContainer.empty();
+                getData(city)
+                console.log('does not exist')
+                console.log(city)
+            }
         });
-    searchButton.on('click', getData);
 }
 
 //function to display UV index: 
@@ -94,8 +95,6 @@ function uvIndexData(lon, lat) {
         .then(function(data) {
             console.log(data)
             console.log(data.current.uvi);
-            //to get the <p> to just be the color need to do below 
-            //ok for that youll want to wrap the actual value in a span inside the p and add the background to the span not the div // 
             //create p element to display UVI
             const uvi = $('<p>UV Index:</p>')
                 // const span, target text into a string, math.floor rounds uvindex to single figure.
@@ -137,47 +136,87 @@ function fiveDayForecast(city) {
         })
         .then(function(data) {
             console.log(data)
-            let count = 0; // start a count to increment with each forecast day
-            for (var i = 0; i < data.list.length; i++) {
-                var dayData = data.list[i]
-                if (dayData.dt_txt.includes("03:00")) {
-                    console.log(dayData.dt_txt)
-                    var unixDate = moment.unix(dayData.dt).utc().format("dddd, MMMM Do YYYY")
-                    console.log(unixDate)
-                    var iconCode = dayData.weather[0].icon;
-                    var iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
+                // let count = 0; // start a count to increment with each forecast day
 
-                    // count++; //increment counter 
-                    // create each section to append daily weather details
-                    let weatherCol = $(`
-                        <div class="col" id="day-forecast">
-                            <h4><strong> ${unixDate}</strong> <img src = "${iconURL}" class ="icon-ig" alt = "weahter-icon"></h4> 
-                            <p><strong>Temperature: ${dayData.main.temp}</p>
-                            <p><strong>Humidity:</strong> ${dayData.main.humidity}</p>
-                            <p><strong>Wind Speed:</strong> ${dayData.wind.speed}</p>
-                        </div>
-                    `);
-                    $('.weather').append(weatherCol); // Append weather div to weather row
+            if ($('#day-forecast').length == 0) {
+                for (var i = 0; i < data.list.length; i++) {
+                    var dayData = data.list[i]
+                    if (dayData.dt_txt.includes("03:00")) {
+                        console.log(dayData.dt_txt)
+                        var unixDate = moment.unix(dayData.dt).utc().format("dddd, MMMM Do YYYY")
+                        console.log(unixDate)
+                        var iconCode = dayData.weather[0].icon;
+                        var iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+                        // create each section to append daily weather details
+                        let weatherCol = $(`
+                            <div class="col mx-3" id="day-forecast">
+                                <h4><strong> ${unixDate}</strong> <img src = "${iconURL}" class ="icon-ig" alt = "weahter-icon"></h4> 
+                                <p><strong>Temperature: ${dayData.main.temp}</p>
+                                <p><strong>Humidity:</strong> ${dayData.main.humidity}</p>
+                                <p><strong>Wind Speed:</strong> ${dayData.wind.speed}</p>
+                            </div>
+                        `);
+                        $('.weather').append(weatherCol);
+                        // Append weather div to weather row
+                    }
                 }
+                let forecastText = "<h2> Five Day Forecast </h2>"
+                $('.weather').prepend(forecastText);
+            } else {
+                $('.weather').empty();
+                fiveDayForecast(city);
             }
         })
 }
 
 
+//function to store city search in local storage. 
+function storeCityHistory(data) {
+    if (data) {
+        var old = localStorage.getItem('city');
+        if (old === null) old = ''; //if nothing in local storage 
+        if (old.indexOf(data) == -1) { //not letting you add the same city twice 
+            localStorage.setItem('city', old + ', ' + data)
+        }
+    }
 
-// const dayDate = moment.unix(data.dt).format("dddd, MMMM Do YYYY")
-// console.log(dayDate)
-// cityName.append(': ' + dayDate)
+}
 
-// run each function on button click
-let searchBtn = $('#button-addon2');
-searchBtn.on('click', function() {
-    let city = $('#newCity').val().toLowerCase();
-    fiveDayForecast(city)
-    getData(city);
-})
+//create function to render to #search-result 
+function addSearch() {
+    var local = localStorage.getItem('city')
+    if (local === null) local = '';
+    var localArray = local.split(', ');
 
-// fiveDayForecast('adelaide');
-//Call functions
-// currentWeather('adelaide');
-// getData();
+    for (var i = 0; i < localArray.length; i++) {
+        if (localArray[i] == '') {
+            $('.searched').addClass('hide')
+        } else {
+            $('#searchResult').append(`<li class ="searched" > ${localArray[i]}</li>`)
+        }
+
+    }
+}
+addSearch();
+
+
+
+
+//create function to hide weather containers unless clicked on 
+//create function to remove old weather and replace with new search result 
+function displayOld() {
+    var oldResults = $('.searched')
+    oldResults.each(function(index, result) {
+        $(this).on('click', function() {
+            var city = $(this).text()
+            getData(city)
+            fiveDayForecast(city)
+            console.log(city)
+
+        })
+
+    })
+}
+
+displayOld();
